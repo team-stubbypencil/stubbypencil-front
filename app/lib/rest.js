4c6a411ccad7fca521a77027aef55cf1f8e2dc58
Changhwa Oh <changhwaoh.co@gmail.com>
2014-04-15 07:57:16 +0900

    백엔드와 통신할 모듈 뼈대

---
+++ app/lib/rest.js
@@ -0,0 +1,19 @@
+var rest = require('restler');
+
+var api = {
+
+    post: function(url, data){
+
+
+    },
+
+    get : function(){
+
+    },
+
+    put : function(){
+
+    }
+};
+
+module.exports = api;
